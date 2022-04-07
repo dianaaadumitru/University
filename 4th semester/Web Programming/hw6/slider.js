1289f753f26edbfa3d68056e9bf1477d2d3ee9ed
dianaaadumitru <dianad2601@gmail.com>
2022-04-07 13:44:19 +0300

    added hw6 web

--- 4th semester/Web Programming/hw6/slider.js
+++ 4th semester/Web Programming/hw6/slider.js
@@ -1,25 +1,39 @@
 $(document).ready(function(){
+
     var picWidth = 200;
     var poz = 0;
+    var popupDiv = document.getElementById("popup-id");
+    var popupImg = document.getElementById("popup-img");
+
+    $("img").click(function(){
+        var img = $(this).attr('src');
+        popupDiv.style.display = 'block';
+        popupImg.src = img;
+        $("li").clearQueue();
+        $("li").stop();
+    });
+
+    popupImg.onclick = function() {
+        popupDiv.style.display = 'none';
+        slide();
+    }
 
     $("li").each(function() {
         poz += picWidth;
         $(this).css("left",poz);
     });
 
-    function slide() {
-        $("li").animate({"left":"+=10px"}, 100, again);
-    }
 
-    function again() {
+    function slide() {
+        $("li").animate({"left":"+=10px"}, 'fast', repeat);
+    }
+    function repeat() {
         var left = $(this).parent().offset().left + $(this).offset().left;
-        console.log("left="+left);
         if (left >= 1600) {
             $(this).css("left",left - 1600);
         }
         slide();
     }
 
-    slide()
-
+    slide();
 });
